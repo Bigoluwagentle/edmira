ead806174fd5118ba244a15db9e31e02a970b56b
bigoluwagentle <olayiwolaolabode06@gmail.com>
2025-10-08 14:01:12 -0700

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <div id={styles.container}>
 
         <motion.div id={styles.mobile} className="mobile"
-          initial={{y:-100, opacity:0}}
+          initial={{y:100, opacity:0}}
           whileInView={{y:0, opacity:1}}
           transition={{duration:1}}
         >
@@ -80,6 +80,82 @@ export default function Home() {
           </aside>
 
         </motion.header>
+
+        <section>
+          <div>
+            <motion.h1
+              initial={{x:100, opacity:0}}
+              whileInView={{x:0,opacity:1}}
+              transition={{duration:1}}
+            >Advancing Medical Learning</motion.h1>
+            <motion.h1
+              initial={{x:-100, opacity:0}}
+              whileInView={{x:0,opacity:1}}
+              transition={{duration:1}}
+            >Through Innovation</motion.h1>
+            <motion.p
+              initial={{y:100, opacity:0}}
+              whileInView={{y:0,opacity:1}}
+              transition={{duration:1}}
+            >EdMira is dedicated to transforming medical education across Africa by providing accessible digital resources that strengthen understanding, improve exam performance, and bridge the gap between theory and clinical practice.</motion.p>
+            <nav>
+              <motion.button
+                initial={{x:-100, opacity:0}}
+                whileInView={{x:0,opacity:1}}
+                transition={{duration:1, type: "spring", stiffness:120, delay:0.5}}
+              >Get Started</motion.button>
+              <motion.button
+                initial={{x:100, opacity:0}}
+                whileInView={{x:0,opacity:1}}
+                transition={{duration:1,type: "spring", stiffness:120, delay:0.5}}
+                onClick={() => {
+                  alert("Anticipate. Coming Soon!")
+                }}
+              >Download App</motion.button>
+            </nav>
+            
+          </div>
+          <div>
+            <motion.img src="/illustration.jpg" alt="" 
+              initial={{scale:0,opacity:0}}
+              whileInView={{scale:1, opacity:1}}
+              transition={{duration:2}}
+            />
+          </div>
+        </section>
+
+        <summary>
+          <section>
+            <motion.nav
+              initial={{x:100,opacity:0}}
+              whileInView={{x:0, opacity:1}}
+              transition={{duration:2}}
+            >
+              <h4>About Us</h4>
+              <motion.p
+                initial={{y:100,opacity:0}}
+                whileInView={{y:0, opacity:1}}
+                transition={{duration:2}}
+              > Edmira is a mobile learning app designed for medical students in Nigerian universities. Our goal is to provide curriculum-aligned, accessible, and interactive study tools that help students:</motion.p>
+              <li>Study smarter and more efficiently. </li>
+              <li> Prepare better for exams and practicals.</li>
+              <li>Bridge the gap between theory and clinical practice. </li>
+              <p>We believe medical students deserve more than textbooks and lecture notes they need digital tools, quizzes, practice banks, animations, and AI-powered study support that make learning easier and engaging. </p>
+            </motion.nav>
+          </section>
+          <aside>
+            <motion.nav
+              initial={{x:-100,opacity:0}}
+              whileInView={{x:0, opacity:1}}
+              transition={{duration:2}}
+            >
+              <h4>Our Vision</h4>
+              <p>To become the leading edtech platform for medical education in Africa, empowering students with digital learning resources that improve knowledge retention, exam performance, and real-world medical practice. </p>
+            </motion.nav>
+          </aside>
+        </summary>
+
+        
 
         <Course/>
 
